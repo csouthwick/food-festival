891410964befd333639d3b5aea5dac4057d003c3
Chris Southwick <csouthwick@live.com>
2020-12-16 02:24:34 -0500

    sync to code snapshot

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const webpack = require("webpack");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const path = require('path');
-const webpack = require('webpack');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
 
-module.exports = {
+
+const config = {
   entry: {
     app: './assets/js/script.js',
     events: './assets/js/events.js',
@@ -10,13 +13,13 @@ module.exports = {
     tickets: './assets/js/tickets.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
@@ -24,7 +27,7 @@ module.exports = {
               name(file) {
                 return '[path][name].[ext]';
               },
-              publicPath: function (url) {
+              publicPath: function(url) {
                 return url.replace('../', '/assets/');
               }
             }
@@ -38,12 +41,28 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
     })
+    // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
   ],
   mode: 'development'
 };
+
+module.exports = config;
